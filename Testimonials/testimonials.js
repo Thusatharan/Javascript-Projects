df7362a834af11ffd71aa28ff3db35af19496d5e
Thusatharan <thusatharan27@gmail.com>
2021-08-14 16:55:14 +0530

    show person function added

--- Testimonials/testimonials.js
+++ Testimonials/testimonials.js
@@ -3,18 +3,21 @@ const reviews = [
     {
         id: 1,
         name: "Michael Scofield",
+        jo: "Software Engineer",
         img: "https://www.hotfootdesign.co.uk/wp-content/uploads/2016/05/d5jA8OZv.jpg",
         description: "photos of customers using your product or service or displaying it in real-life situations demonstrates an extra level of effort that not only proves their loyalty to your business, but it allows other consumers to see your goods in action, which can help them make informed shopping decisions."
     },
     {
         id: 2,
         name: "John Smith",
+        job: "Arrchitect",
         img: "https://preview.keenthemes.com/metronic-v4/theme/assets/pages/img/avatars/team1.jpg",
         description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable"
     },
     {
         id: 3,
         name: "Sanjay Arjun",
+        job: "Content Witter",
         img: "https://pbs.twimg.com/profile_images/974736784906248192/gPZwCbdS.jpg",
         description: "I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness."
     },
@@ -23,14 +26,30 @@ const reviews = [
 
 // getElements
 const img = document.getElementById("user-img");
-const name = document.getElementById("name");
+const userName = document.getElementById("name");
 const job = document.getElementById("job");
 const description = document.getElementById("review-desc");
 
 const prevButton = document.querySelector(".previous");
 const nextButton = document.querySelector(".next");
 
+// Starting item
+let currentitem = 2;
+
 // Load initial Items
 window.addEventListener("DOMContentLoaded", function () {
-    console.log("Document Loaded");
-})
+    showPerson(currentitem);
+
+});
+
+
+function showPerson(person) {
+    const item = reviews[person];
+    console.log(item);
+    img.src = item.img;
+    userName.textContent = item.name;
+    job.textContent = item.job;
+    description.textContent = item.description;
+};
+
+
