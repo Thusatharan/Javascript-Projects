6e3aaf9e00eaeb8a3930b0cb058d946af1a1758b
Thusatharan <thusatharan27@gmail.com>
2021-08-14 10:13:27 +0530

    simple color

---
+++ Color Flipper/colorFlipper.js
@@ -0,0 +1,23 @@
+const colors = [
+    '#9cbff7',
+    '#9cf7ae',
+    '#f79cd1',
+    '#ffc8a8',
+    '#ffa8af',
+];
+
+const button = document.getElementById("#btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function name() {
+    const randomNumber = getRandom();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    color.style.color = colors[randomNumber];
+}
+);
+
+function getRandom() {
+    return Math.floor(Math.random() * colors.length);
+}
