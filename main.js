bad9bd0233922bd519c9c55e8aea25b310a8272d
Thusatharan <thusatharan27@gmail.com>
2021-08-18 00:21:28 +0530

    modified main page

---
+++ main.js
@@ -0,0 +1,33 @@
+// const projects = [
+//     {
+//         id: 1,
+//         title: "Color Flipper",
+//         link: "Color Flipper/colorFlipper.html",
+//         image: "./img/colorFlipper.png",
+//     },
+
+//     {
+//         id: 2,
+//         title: "Counter",
+//         link: "Counter/counter.html",
+//         image: "./img/counter.png",
+//     },
+
+//     {
+//         id: 3,
+//         title: "User Reviews",
+//         link: "Testimonials/testimonials.html",
+//         image: "./img/userReview.png",
+//     }
+// ];
+
+// const img = document.getElementById("image");
+// const link = document.getElementById("cardContainer");
+// const title = document.querySelector(".desc");
+
+// window.addEventListener("DOMContentLoaded", function () {
+
+//     for (let i = 0; i <= projects.length - 1; i++) {
+//         img.src = projects[i].image;
+//     }
+// });
